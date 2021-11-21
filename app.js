3a471ce37fa8264d01b1d201d829fd9e5fd8d162
Hitesh Pagare <imhietesh@gmail.com>
2021-11-21 20:16:51 +0530

    added the cotroller and router for home

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const routes = require('./routes/index');
 const app = express();
 
 const port = 8000;
+
+
+//use express router
+
+app.use('/',routes);
 
 app.listen(port,function(err){
     if(err){
